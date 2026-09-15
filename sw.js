4c587a6faf9450c77cc83d369e5fe64dacbb2a34
Rio9898 <egyypt09@gmail.com>
2026-09-15 06:55:35 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE='rwl-web-v7';
+const STATIC=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./assets/rwl-icon.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(e.request.method!=='GET'||u.hostname.includes('workers.dev')||u.hostname.includes('tmdb.org')) return;
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
+});
